ef5be5d7b82f7e762ebb6d7503f69d171fad0179
Florencia Gomez <Florxgomez@gmail.com>
2019-11-06 17:38:54 -0300

    modified footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import './Footer.css';
-
+import React from "react";
+import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCodepen,
+  faLinkedin,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
 const Footer = () => (
   <footer>
-  <div className="footer-items">
-    <ul>
-      <li className="footer-item">
-        <i className="fa fa-linkedin"></i>
-      </li>
-      <li className="footer-item">
-        <i className="fa fa-github-square"></i>
-      </li>
-    <li className="footer-item">
-      <i className="fa fa-codepen"></i>
-      </li>
-  </ul>
-  Made with &hearts; by Flor
- </div>
- </footer>
-)
+    <div className="footer-items">
+      <ul>
+        <li className="footer-item">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </li>
+        <li className="footer-item">
+          <FontAwesomeIcon icon={faGithub} />
+        </li>
+        <li className="footer-item">
+          <FontAwesomeIcon icon={faCodepen} />
+        </li>
+      </ul>
+      Made with &hearts; by Flor
+    </div>
+  </footer>
+);
 
 export default Footer;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,50 +1,56 @@
-import React from 'react';
-import './Header.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCodepen,
+  faLinkedin,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
 
 class Header extends React.Component {
-
-  render(){
+  render() {
     return (
-      <header className={`top ${this.props.border ? 'top-border' : ''}`}>
-    <h1>
-      <Link className='home-link' to='/'>
-        <img src='https://s.cdpn.io/profiles/user/2184540/80.jpg?1569736779' alt="Flor Icon"/>
-        <span>Florencia Gomez</span>
-      </Link>
-    </h1>
-<nav className='nav-menu'>
-  <ul id='menu-header'>
-    <li className='menu-item'>
-      <Link to='/aboutme'>About Me</Link>
-    </li>
-    <li className='menu-item'>
-      <Link to='/projects'>Projects</Link>
-    </li>
-    <li className='menu-item'>
-      <Link to='/skills'>My Skills</Link>
-    </li>
-    <li className='menu-item'>
-      <Link to='/contact'>Contact</Link>
-    </li>
-  </ul>
-  <ul className='header-social'>
-    <li className='social-item'>
-      <i className="fa fa-linkedin"></i>
-    </li>
-    <li className='social-item'>
-      <i className="fa fa-github-square"></i>
-    </li>
-  <li className='social-item'>
-      <i className="fa fa-codepen"></i>
-  </li>
-  </ul>
-</nav>
-
-</header>
-    )
+      <header className={`top ${this.props.border ? "top-border" : ""}`}>
+        <h1>
+          <Link className="home-link" to="/">
+            <img
+              src="https://s.cdpn.io/profiles/user/2184540/80.jpg?1569736779"
+              alt="Flor Icon"
+            />
+            <span>Florencia Gomez</span>
+          </Link>
+        </h1>
+        <nav className="nav-menu">
+          <ul id="menu-header">
+            <li className="menu-item">
+              <Link to="/aboutme">About Me</Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/skills">My Skills</Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <ul className="header-social">
+            <li className="social-item">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </li>
+            <li className="social-item">
+              <FontAwesomeIcon icon={faGithub} />
+            </li>
+            <li className="social-item">
+              <FontAwesomeIcon icon={faCodepen} />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
   }
-  
 }
 
 export default Header;
