d00c79da97d23c0abfb5963ed49b0e6b131407cd
Flor <florxgomez@gmail.com>
2019-11-23 00:31:41 -0300

    inline style

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,20 +12,30 @@ const Footer = () => (
       <div className="footer-items">
         <ul>
           <li className="footer-item">
-            <a className="footer-link"
+            <a
+              className="footer-link"
               href="https://www.linkedin.com/in/florenciaggomez/"
+              style={{ color: '#313131' }}
               target="_blank"
             >
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
           </li>
           <li className="footer-item">
-            <a href="https://github.com/florxgomez" target="_blank">
+            <a
+              href="https://github.com/florxgomez"
+              target="_blank"
+              style={{ color: '#313131' }}
+            >
               <FontAwesomeIcon icon={faGithub} />
             </a>
           </li>
           <li className="footer-item">
-            <a href="https://codepen.io/greenflor_" target="_blank">
+            <a
+              href="https://codepen.io/greenflor_"
+              target="_blank"
+              style={{ color: '#313131' }}
+            >
               <FontAwesomeIcon icon={faCodepen} />
             </a>
           </li>
