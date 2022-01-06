feff7ff0d43dd0ca7ac41fcf7068542222ab8244
Florencia Gomez <florxgomez@gmail.com>
2022-01-06 16:23:56 -0300

    added a new project

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,53 +1,65 @@
-import React, { Fragment } from "react";
-import { faGlobe, faCode } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Fragment } from 'react';
+import { faGlobe, faCode } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faJs,
   faReact,
   faNodeJs,
   faCss3,
-  faBootstrap
-} from "@fortawesome/free-brands-svg-icons";
-import firebaseIcon from "../../images/icons/firebase.png";
-import reduxIcon from "../../images/icons/redux.png";
-import materialIcon from "../../images/icons/material-ui-logo.png";
+  faBootstrap,
+} from '@fortawesome/free-brands-svg-icons';
+import firebaseIcon from '../../images/icons/firebase.png';
+import reduxIcon from '../../images/icons/redux.png';
+import materialIcon from '../../images/icons/material-ui-logo.png';
+import nextjsIcon from '../../images/icons/nextjs.png';
+import prismaIcon from '../../images/icons/prisma.png';
+import postgressIcon from '../../images/icons/postgress.png';
+import vercelIcon from '../../images/icons/vercel.png';
 
-import "./Project.css";
+import './Project.css';
 
-const Project = props => {
-  const renderFA = icon => {
+const Project = (props) => {
+  const renderFA = (icon) => {
     switch (icon) {
-      case "fajs":
+      case 'fajs':
         return faJs;
-      case "fareact":
+      case 'fareact':
         return faReact;
-      case "fanode":
+      case 'fanode':
         return faNodeJs;
-      case "facss":
+      case 'facss':
         return faCss3;
-      case "faboot":
+      case 'faboot':
         return faBootstrap;
       default:
         return null;
     }
   };
 
-  const renderOtherIcon = el => {
+  const renderOtherIcon = (el) => {
     switch (el.icon) {
-      case "firebase":
+      case 'firebase':
         return firebaseIcon;
-      case "redux":
+      case 'redux':
         return reduxIcon;
-      case "material":
+      case 'material':
         return materialIcon;
+      case 'nextjs':
+        return nextjsIcon;
+      case 'prisma':
+        return prismaIcon;
+      case 'postgress':
+        return postgressIcon;
+      case 'vercel':
+        return vercelIcon;
       default:
         return null;
     }
   };
 
-  const renderIcons = icons =>
-    icons.map(el =>
-      el.icon.startsWith("fa") ? (
+  const renderIcons = (icons) =>
+    icons.map((el) =>
+      el.icon.startsWith('fa') ? (
         <div class="icon">
           <FontAwesomeIcon
             icon={renderFA(el.icon)}
@@ -57,7 +69,6 @@ const Project = props => {
         </div>
       ) : (
         <div class="icon">
-          {console.log(renderOtherIcon(el))}
           <img
             class={`${el.icon}-size`}
             src={renderOtherIcon(el)}
@@ -80,10 +91,20 @@ const Project = props => {
           <p>{desc}</p>
         </div>
         <div class="box-links">
-          <a href={website} class="box-link" target="_blank" rel="noopener noreferrer">
+          <a
+            href={website}
+            class="box-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faGlobe} size="lg" />
           </a>
-          <a href={codeUrl} class="box-link" target="_blank" rel="noopener noreferrer">
+          <a
+            href={codeUrl}
+            class="box-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faCode} size="lg" />
           </a>
         </div>
--- src/projectList.js
+++ src/projectList.js
@@ -1,86 +1,111 @@
 const proyectList = [
   {
     id: 1,
-    title: "Social Interact",
-    desc: "Full-Stack social media application",
+    title: 'Trax: Music App',
+    desc: 'Full-Stack music application',
     stack: [
       {
-        icon: "fajs",
-        color: "rgb(240, 219, 79)"
+        icon: 'fareact',
+        color: 'rgba(97, 219, 251)',
       },
       {
-        icon: "fareact",
-        color: "rgba(97, 219, 251)"
+        icon: 'nextjs',
       },
       {
-        icon: "fanode",
-        color: "#6cc24a"
+        icon: 'prisma',
       },
       {
-        icon: "redux"
+        icon: 'postgress',
       },
       {
-        icon: "material"
+        icon: 'vercel',
       },
-      {
-        icon: "firebase"
-      }
     ],
-    codeUrl: "https://github.com/florxgomez/social-app-client",
-    website: "https://social-interact.herokuapp.com/"
+    codeUrl: 'https://github.com/florxgomez/music-app',
+    website: 'https://music-app-green-ten.vercel.app/signin',
   },
+  /*  {
+    id: 1,
+    title: 'Social Interact',
+    desc: 'Full-Stack social media application',
+    stack: [
+      {
+        icon: 'fajs',
+        color: 'rgb(240, 219, 79)',
+      },
+      {
+        icon: 'fareact',
+        color: 'rgba(97, 219, 251)',
+      },
+      {
+        icon: 'fanode',
+        color: '#6cc24a',
+      },
+      {
+        icon: 'redux',
+      },
+      {
+        icon: 'material',
+      },
+      {
+        icon: 'firebase',
+      },
+    ],
+    codeUrl: 'https://github.com/florxgomez/social-app-client',
+    website: 'https://social-interact.herokuapp.com/',
+  }, */
   {
     id: 2,
-    title: "Crwn-Clothing",
-    desc: "E-commerce app with simulated payments",
+    title: 'Crwn-Clothing',
+    desc: 'E-commerce app with simulated payments',
     stack: [
       {
-        icon: "fareact",
-        color: "rgba(97, 219, 251)"
+        icon: 'fareact',
+        color: 'rgba(97, 219, 251)',
       },
       {
-        icon: "redux"
+        icon: 'redux',
       },
       {
-        icon: "firebase"
-      }
+        icon: 'firebase',
+      },
     ],
-    codeUrl: "https://github.com/florxgomez/crwn-clothing",
-    website: "http://crwnclth.herokuapp.com/"
+    codeUrl: 'https://github.com/florxgomez/crwn-clothing',
+    website: 'http://crwnclth.herokuapp.com/',
   },
   {
     id: 3,
-    title: "LyricsFinder",
-    desc: "Lyrics finder application",
+    title: 'LyricsFinder',
+    desc: 'Lyrics finder application',
     stack: [
       {
-        icon: "fareact",
-        color: "rgba(97, 219, 251)"
+        icon: 'fareact',
+        color: 'rgba(97, 219, 251)',
       },
       {
-        icon: "faboot",
-        color: "rgb(86, 61, 124)"
-      }
+        icon: 'faboot',
+        color: 'rgb(86, 61, 124)',
+      },
     ],
-    codeUrl: "https://github.com/florxgomez/lyrics-finder",
-    website: "https://florxgomez.github.io/lyrics-finder/"
+    codeUrl: 'https://github.com/florxgomez/lyrics-finder',
+    website: 'https://florxgomez.github.io/lyrics-finder/',
   },
   {
     id: 4,
-    title: "Reduxtagram",
-    desc: "Frontend implementation for an Instagram based app",
+    title: 'Reduxtagram',
+    desc: 'Frontend implementation for an Instagram based app',
     stack: [
       {
-        icon: "fareact",
-        color: "rgba(97, 219, 251)"
+        icon: 'fareact',
+        color: 'rgba(97, 219, 251)',
       },
       {
-        icon: "redux"
-      }
+        icon: 'redux',
+      },
     ],
-    codeUrl: "https://github.com/florxgomez/reduxstagram",
-    website: "https://florxgomez.github.io/reduxstagram/"
-  }
+    codeUrl: 'https://github.com/florxgomez/reduxstagram',
+    website: 'https://florxgomez.github.io/reduxstagram/',
+  },
 ];
 
 export default proyectList;
